db27edc6f6dbf5b85b1b716227e62d906c5efe90
KAIQUE <cg3027481@labA204m23.IFSP>
2024-08-06 08:35:28 -0300

    aplicando o header

---
+++ src/components/Header/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './style.css';
+
+const Header = () => {
+    return (
+        <header>
+            <div className='header center'>
+                <picture>
+                    <img src='./assets/logo.png' alt='logo fusca'/>
+                </picture>
+
+                <nav>
+                    <ul>
+                        <li>
+                            <a href= '#'>Produtos</a>
+                        </li>
+
+                        <li>
+                            <a href= '#'>Sobre</a>
+                        </li>
+
+                        <li>
+                            <a href= '#'>Contatos</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
