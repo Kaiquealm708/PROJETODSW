95990ade19da0656053a0062b244111a5d8a1b6f
Kaique Almeida <kaique.batista@aluno.ifsp.edu.br>
2024-08-27 08:36:50 -0300

    Co-authored-by: caio-pixel <caio-pixel@users.noreply.github.com> Co-authored-by: ImdsLenda <ImdsLenda@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import SecaoCapa from './components/SecaoCapa';
 
 function App() {
   return (
     <div className="App">
      <Header/>
+     <SecaoCapa/>
     </div>
   );
 }
---
+++ src/components/SecaoCapa/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './style.css';
+
+const SecaoCapa = () => {
+    return (
+        <section classname ='secao-capa'>
+            <div classname='capa center'>
+                <div className='texto-capa'>
+                    <p>Preços baixos em</p>
+                    <h1>carros semi novos e novos</h1>
+                    <p>voce so encontra aqui</p>                
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default SecaoCapa;
